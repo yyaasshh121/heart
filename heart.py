691616ea3e3bf8b8a2bcb06f2b8f17d7a211e67a
yyaasshh121 <121356019+yyaasshh121@users.noreply.github.com>
2022-12-24 23:32:03 +0530

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,17 @@
+import turtle
+
+t=turtle.Turtle()
+
+t.color("red")
+t.begin_fill()
+t.color("red")
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+#left(120)]
+
+t.setheading(60)
+t.circle(-90,200)
+t.forward(180)
+t.end_fill()
+a=input()
